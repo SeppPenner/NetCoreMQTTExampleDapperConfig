40420a8d28546da86e475a6cbcde58077ab145dc
SeppPenner <HansM2013@gmx.de>
2021-02-02 21:25:42 +0100

    NetCoreMQTTExampleDapperConfig: Removed not needed import.

--- src/NetCoreMQTTExampleDapperConfig/Program.cs
+++ src/NetCoreMQTTExampleDapperConfig/Program.cs
@@ -12,7 +12,6 @@ namespace NetCoreMQTTExampleDapperConfig
     using System.IO;
     using System.Reflection;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
 
