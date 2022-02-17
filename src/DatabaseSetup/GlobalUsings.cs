e0007f54c526e70f370d7abc34bf1cae02bb7dc9
SeppPenner <HansM2013@gmx.de>
2022-02-17 19:20:43 +0100

    Smaller changes.

--- src/DatabaseSetup/GlobalUsings.cs
+++ src/DatabaseSetup/GlobalUsings.cs
@@ -1,4 +1,4 @@
-#pragma warning disable IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
 global using Storage;
 global using Storage.Database;
 global using Storage.Enumerations;
@@ -6,4 +6,4 @@ global using Storage.Repositories.Implementation;
 global using Storage.Repositories.Interfaces;
 
 global using Microsoft.AspNetCore.Identity;
-#pragma warning restore IDE0065 // Die global using-Anweisung wurde falsch platziert.
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
